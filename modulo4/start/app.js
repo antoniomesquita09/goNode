a90cd80d44b84c2aed2beea61605f0912e366d28
antoniomesquita09 <antoniomesquita09@gmail.com>
2019-05-16 08:10:26 -0300

    modulo4

--- modulo4/start/app.js
+++ modulo4/start/app.js
@@ -17,7 +17,11 @@ const providers = [
   '@adonisjs/cors/providers/CorsProvider',
   '@adonisjs/lucid/providers/LucidProvider',
   '@adonisjs/mail/providers/MailProvider',
-  '@adonisjs/framework/providers/ViewProvider'
+  '@adonisjs/framework/providers/ViewProvider',
+  '@adonisjs/validator/providers/ValidatorProvider',
+  '@adonisjs/antl/providers/AntlProvider',
+  '@adonisjs/redis/providers/RedisProvider',
+  'adonis-kue/providers/KueProvider'
 ]
 
 /*
@@ -29,7 +33,10 @@ const providers = [
 | Providers for migrations, tests etc.
 |
 */
-const aceProviders = ['@adonisjs/lucid/providers/MigrationsProvider']
+const aceProviders = [
+  '@adonisjs/lucid/providers/MigrationsProvider',
+  'adonis-kue/providers/CommandsProvider'
+]
 
 /*
 |--------------------------------------------------------------------------
@@ -54,5 +61,14 @@ const aliases = {}
 |
 */
 const commands = []
+/*
+|--------------------------------------------------------------------------
+| jobs - criado por mim
+|--------------------------------------------------------------------------
+|
+| Here you store your queue jobs
+|
+*/
+const jobs = ['App/Jobs/NewTaskMail']
 
-module.exports = { providers, aceProviders, aliases, commands }
+module.exports = { providers, aceProviders, aliases, commands, jobs }
